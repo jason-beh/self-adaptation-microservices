fb3c158a011d62ed43b7c9af08372441a51f527e
Jason Beh <behjieshen@gmail.com>
2022-08-18 23:01:55 +0930

    Added scaling based on memory metrics

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 TARGET_AVERAGE_UTILIZATION = 50
+TARGET_AVERAGE_MEMORY = 13000000000
 
 def main():
     spec = json.loads(sys.stdin.read())
@@ -17,11 +18,12 @@ def evaluate(spec):
     metric_value = json.loads(spec["metrics"][0]["value"])
     current_replicas = metric_value["current_replicas"]
     average_utilization = metric_value["average_utilization"]
+    average_memory = metric_value["average_memory"]
 
     # Calculate target replicas, increase by 1 if utilization is above target, decrease by 1 if utilization is below
     # target
     target_replicas = current_replicas
-    if average_utilization > TARGET_AVERAGE_UTILIZATION:
+    if average_utilization > TARGET_AVERAGE_UTILIZATION or average_memory > TARGET_AVERAGE_MEMORY:
         target_replicas += 1
     else:
         target_replicas -= 1
