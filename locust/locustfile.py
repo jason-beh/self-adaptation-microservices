b5fe058ec483f9c4c6109cd7eea5436eaf162a55
Jason Beh <behjieshen@gmail.com>
2022-09-15 15:08:49 +0930

    Configured locust with multiple workers

---
+++ locust/locustfile.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task, between, FastHttpUser
+
+class HelloWorldUser(FastHttpUser):
+    wait_time = between(1.0,1.0)
+
+    @task
+    def hello_world(self):
+        self.client.get("/")
+        self.client.get("/auth/login")
+        self.client.get("/results")
