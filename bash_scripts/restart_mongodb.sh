644fce77015571b2e319bc33fd67acc8a8963153
Jason Beh <behjieshen@gmail.com>
2022-10-15 20:09:35 +1030

    Update mongodb script

--- bash_scripts/restart_mongodb.sh
+++ bash_scripts/restart_mongodb.sh
@@ -2,6 +2,6 @@
 
 cd /Users/jasonbeh/Desktop/code/custom-autoscalars/project/bitnami &&
 
-helm install my-release -f values.yaml bitnami/mongodb --namespace app &&
+helm upgrade --install my-release -f values.yaml bitnami/mongodb --namespace app &&
 
 echo "Restarted mongodb"
