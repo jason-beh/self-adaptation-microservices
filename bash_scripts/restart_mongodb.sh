22589eef0ba2a552383756a7ae33ebaa213768b2
Jason Beh <behjieshen@gmail.com>
2022-10-18 13:12:45 +1030

    Cleanup mongodb

--- bash_scripts/restart_mongodb.sh
+++ bash_scripts/restart_mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /Users/jasonbeh/Desktop/code/custom-autoscalars/project/bitnami &&
+cd /Users/jasonbeh/Desktop/code/custom-autoscalars/project/mongodb &&
 
 helm upgrade --install my-release -f values.yaml bitnami/mongodb --namespace app &&
 
